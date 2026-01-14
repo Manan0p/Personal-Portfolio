e462b368de661e4e2d266d141e9a58af3d938dc5
Manan <imananop@gmail.com>
2026-01-14 11:16:50 +0530

    font chaNGE

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets: ["latin"]});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
         <ThemeProvider
             attribute="class"
--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,14 @@ export default function Home() {
     <div>
       <header className="fixed top-0 w-full border-b z-50 backdrop-blur-md supports-backdrop-filter:bg-background/60">
         <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <span className="inline-block h-3 w-3 rounded-full bg-linear-to-tr from-pink-500 to-purple-500" />
+            <span className="font-inter text-sm font-medium tracking-tight text-white">
+              Manan Lall
+            </span>
+          </div>
+
+
           <div>
             <Button href="/" className="text-lg font-bold">
               Home
