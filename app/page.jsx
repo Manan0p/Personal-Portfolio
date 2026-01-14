d049af59b142e0c1350eba8a0c404fea946d76d1
Manan <imananop@gmail.com>
2026-01-14 19:36:10 +0530

    2nd

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./toggle";
-import { Card } from "@/components/ui/card";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
@@ -75,6 +75,18 @@ export default function Home() {
                   priority
                   className="w-full max-w-3xl drop-shadow-[0_20px_60px_rgba(0,0,0,0.65)]"
                 />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="w-full py-7 md:py-14 lg:py-14 bg-background">
+          <div className="container mx-auto px-4 md:px-6">
+            <div>
+              <h2 className="text-3xl font-bold tracking-tighter text-center mb-4">Featured Projects</h2>
+              <div className="text-sm md:text-lg lg:text-xl text-center text-muted-foreground tracking-wide">
+                Innovative Solutions I've Built
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto">
               </div>
             </div>
           </div>
