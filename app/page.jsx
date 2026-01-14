283e3cd3ca17fc8704970c63b2dc8c0823b8d4fd
Manan <imananop@gmail.com>
2026-01-14 21:32:16 +0530

    raw data creation for cards

--- app/page.jsx
+++ app/page.jsx
@@ -81,14 +81,27 @@ export default function Home() {
         </section>
         <section className="w-full py-7 md:py-14 lg:py-14 bg-background">
           <div className="container mx-auto px-4 md:px-6">
-            <div>
-              <h2 className="text-3xl font-bold tracking-tighter text-center mb-4">Featured Projects</h2>
-              <div className="text-sm md:text-lg lg:text-xl text-center text-muted-foreground tracking-wide">
+            <div className="text-center">
+              <h2 className="text-4xl font-bold tracking-wide inline-block bg-[linear-gradient(90deg,#E5F0FF,#CBD5E1,#A5B4FC)] text-transparent bg-clip-text mb-4">Featured Projects</h2>
+              <div className="text-sm md:text-lg lg:text-xl text-muted-foreground tracking-wide">
                 Innovative Solutions I've Built
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto">
               </div>
             </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto">{features.map((feature,index)=>{
+              return(
+                <Card key={index} className={"border-2 hover:border-primary transition-colors duration-300"}>
+                  <CardContent className={"text-center flex flex-col items-center"}>
+                    <div className="flex flex-col items-center justify-center">
+                      {feature.icon}
+                      <h3 className="text-xl font-bold mb-2">{feature.title}</h3>
+                      <p className="text-muted-foreground">{feature.description}</p>
+                    </div>
+                  </CardContent>
+                </Card>
+              )
+            })}</div>
           </div>
         </section>
       </main>
