5857c91e5f442f091067371870f324b7ad7f1cc7
Manan <imananop@gmail.com>
2026-01-15 20:59:18 +0530

    raw files for contact

---
+++ Data/contact.js
@@ -0,0 +1,19 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
+export const contact = [
+  {
+    id: "1",
+    logo: <Mail className="w-16 h-16 text-primary" />,
+    title: "workfurmanan@gmail.com",
+  },
+  {
+    id: "2",
+    logo:<Github className="w-16 h-16 text-primary" />,
+    title: "github.com/Manan0p",
+  },
+  {
+    id: "3",
+    logo: <Linkedin className="w-16 h-16 text-primary" />,
+    title: "linkedin.com/in/mananlall",
+  },
+];
