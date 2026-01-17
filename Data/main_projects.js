91615353da7cb3aa4a61235b3b2faa71e112e179
Manan <imananop@gmail.com>
2026-01-18 01:20:41 +0530

    try

--- Data/main_projects.js
+++ Data/main_projects.js
@@ -3,7 +3,7 @@ export const main_projects = [
         title: "Splitr",
         info: "Smart Expense Splitting",
         description: "An AI-powered expense-sharing web app that makes splitting bills, tracking balances, and settling up effortless for users.",
-        image: "/splitr-mockup.png",
+        image: "/mp/splitr-mockup.png",
         live_link: "https://splitr-sigma-eight.vercel.app/",
         github_link: "https://github.com/Manan0p/splitr"
     }
