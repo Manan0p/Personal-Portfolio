c07c4498ce6f90efad52359674d1ca0d461079f5
Manan <imananop@gmail.com>
2026-01-17 16:46:33 +0530

    raw data for projects

---
+++ Data/showcase.js
@@ -0,0 +1,27 @@
+
+export const showcase = [
+  {
+    title: "Splitr",
+    description:
+      "A smart, intuitive, modern expense-sharing app for splitting bills and managing group expenses.",
+    stack: ["Next.js", "Tailwind CSS", "React", "Vercel", "Convex"],
+  },
+  {
+    title: "Sensai",
+    description:
+      "An AI-powered career assistant for cover letters, interview practice, reumes and industry insights.",
+    stack: ["Next.js", "Tailwind CSS", "React", "Vercel", "PostgreSQL"],
+  },
+  {
+    title: "JobGuard AI",
+    description:
+      "A smart, reliable, user focused platform that helps users identify and avoid fraudulent job postings..",
+    stack: ["Flask", "Scikit-learn", "SQLite", "Joblib", "Gunicorn"],
+  },
+  {
+    title: "DealDrop",
+    description:
+      "A smart price-tracking assistant that monitors products and sends instant alerts when prices drop.",
+    stack: ["React", "Next.js", "Tailwind CSS", "Supabase", "Firecrawl"],
+  },
+];
