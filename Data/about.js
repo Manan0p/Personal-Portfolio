a268890a6c431ae80b6a00a74f5175a5de9198ef
Manan <imananop@gmail.com>
2026-01-21 20:53:38 +0530

    raw data for about

---
+++ Data/about.js
@@ -0,0 +1,14 @@
+import { Layers } from "lucide-react";
+
+export const card = [
+    {
+        icon: "final.png",
+        title: "AI/ML Engineering",
+        description: "Develop machine learning models that solve real world challenges.",
+    },
+    {
+        icon: <Layers className="w-20 h-20 md:w-24 md:h-24 text-violet-400" />,
+        title: "Full-Stack Development",
+        description: "Build responsive, scalable and secure web applications.",
+    }
+]
