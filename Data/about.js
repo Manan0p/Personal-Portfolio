df2606ad8954dc8a429a45c3549fe6ff34129a9b
Manan <imananop@gmail.com>
2026-01-21 20:59:24 +0530

    new icon

--- Data/about.js
+++ Data/about.js
@@ -1,8 +1,8 @@
-import { Layers } from "lucide-react";
+import { BrainCircuit, Layers } from "lucide-react";
 
 export const card = [
     {
-        icon: "final.png",
+        icon: <BrainCircuit className="w-20 h-20 md:w-24 md:h-24 text-violet-400" />,
         title: "AI/ML Engineering",
         description: "Develop machine learning models that solve real world challenges.",
     },
