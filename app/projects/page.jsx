74dbd70a30d8d88e7988aade5cb7e30d41a4c362
Manan <imananop@gmail.com>
2026-01-26 23:28:27 +0530

    priject pg

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -8,7 +8,7 @@ import { caseStudies } from '@/Data/case_studies'
 const page = () => {
   return (
     <div>
-      <section className="relative noise w-full  pt-12 md:pt-20 lg:pt-24 pb-0 bg-gradient-to-br from-[#0c0f1c] via-[#0a0d18] to-[#070912] border-b border-white/10">
+      <section className="relative noise w-full pt-14 md:pt-20 lg:pt-24 pb-0 bg-gradient-to-br from-[#0c0f1c] via-[#0a0d18] to-[#070912] border-b border-white/10">
         <div className="pointer-events-none absolute inset-0 z-0">
           <div className="absolute right-[-10%] bottom-[-35%] h-[680px] w-[680px] rounded-full bg-[radial-gradient(circle_at_30%_30%,
                           rgba(236,72,153,0.85),rgba(168,85,247,0.6),rgba(99,102,241,0.35),transparent_65%)] blur-[140px]"/>
